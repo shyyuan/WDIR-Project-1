11492f6289252e5729eb86a63ca6f5eb4d86961e
029281 <sheilayuan@C02T511YHF1T.home>
2017-03-24 17:54:23 -0400

    Created cards and append to DOM element

--- js/app.js
+++ js/app.js
@@ -3,6 +3,33 @@ console.log('Project 1 js file is connected');
 $(function(){
   console.log('Inside window onload');
 
-
-  
 }) /// End of Window onload
+
+var level = 1;
+var live = 2;
+var numCards = Math.pow(level*2,2);
+var $board = $('#gameBoard');
+
+
+var generateBoard = function(num){
+  // set board width and heigh based on number of cards
+  var boardWidth = level*2*85+'px';
+  var boardHeigth = level*2*115+'px';
+
+  $board.css({'width':boardWidth,'height':boardHeigth});
+
+
+  for (var i=1; i<=num; i++){
+    //console.log('inside loop ' + i);
+    // create card div, add id and class
+    var $card = $('<div>').attr('id','card'+i).addClass('card').text(i);
+    console.log($card);
+    // append to gameBoard div
+    $board.append($card);
+
+  } // end of for loop
+
+
+} // end of generateQuilt
+
+generateBoard(numCards);
