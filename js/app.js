c61cfb38385264bca5e5b5f6c0bf3b0ebc802c6f
029281 <sheilayuan@C02T511YHF1T.home>
2017-03-27 16:38:31 -0400

    added dropdown selection that allowed player to select card type. Set event listener

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ console.log('Project 1 js file is connected');
 // Window onloader ***********************************
 $(function(){
   // Event Listener
+  $('#type').on('change', APP.setType);
   $('#resetLevel').on('click', UI.resetLevel);
   $('#resetGame').on('click', UI.resetGame);
 
@@ -14,6 +15,7 @@ var APP = {
   live: 0, //this.level+2,
   numCards: 0, //Math.pow((this.level+1)*2,2);,
   $board: $('#gameBoard'),
+  $type: 'number', // default
   width: 80,
   height: 80,
   playerScore: 0,
@@ -27,7 +29,11 @@ var APP = {
   // Math.floor(Math.random * (max-min+1)) + min
   generateNum: function(max,min){
     return Math.floor(Math.random()* (max-min+1)) + min;
-  } // end of generateNum
+  }, // end of generateNum
+  setType: function(){
+    APP.$type = $('#type option:selected').val(),
+    console.log('Selected Type ' + APP.$type);
+  }
 } // End of
 // =======================================================
 
@@ -111,25 +117,26 @@ var UI = {
   // 3 3 3 3 3 3
   // flip the card that clicked
   flipCard: function(){
+    if (APP.live !== 0){
+        $('#feedback h4').text('Click another card').css('color','black');
 
-    $('#feedback h4').text('Click another card').css('color','black');
+        var tempP = $(this).attr('id').replace('card','');
+        var value = APP.gameCards.filter(function(element){
+          return element.cardPosition == tempP;
+        })[0].cardValue;
 
-    var tempP = $(this).attr('id').replace('card','');
-    var value = APP.gameCards.filter(function(element){
-      return element.cardPosition == tempP;
-    })[0].cardValue;
+        if (APP.gameCardsClicked.filter(function(element){
+          return element == tempP
+        }).length === 0 ) {
 
-    if (APP.gameCardsClicked.filter(function(element){
-      return element == tempP
-    }).length === 0 ) {
-
-      var tempArr = [tempP,value];
-      APP.cardClicked.push(tempArr);
-      $(this).text(value).css('background','lightgreen')
-      //console.log('Card clicked '+cardClicked);
-      if (APP.cardClicked.length === 2){
-        UI.isMatch(APP.cardClicked);
-      }
+          var tempArr = [tempP,value];
+          APP.cardClicked.push(tempArr);
+          $(this).text(value).css('background','lightgreen')
+          //console.log('Card clicked '+cardClicked);
+          if (APP.cardClicked.length === 2){
+            UI.isMatch(APP.cardClicked);
+          }
+        }
     }
   }, // End of flipCard function
   // 4 4 4 4 4 4 4
@@ -148,6 +155,11 @@ var UI = {
       if (APP.gameCardsClicked.length === APP.numCards) {
         console.log('Level round over, move to next level');
         setTimeout(function() {UI.levelUp();},1000);
+      } else {
+        setTimeout(function() {updateFeedback();}, 700);
+        var updateFeedback = function() {
+          $('#feedback h4').text('Click a card').css('color','black');
+        }
       }
     } else {
       console.log('no match');
@@ -165,7 +177,11 @@ var UI = {
         $('#card'+twoCards[0][0]).append(APP.questionMarkImg);
         $('#card'+twoCards[1][0]).text('').css('background','yellow');
         $('#card'+twoCards[1][0]).append(APP.questionMarkImg);
-        $('#feedback h4').text('Click a card').css('color','black');
+        if (APP.live !== 0){
+          $('#feedback h4').text('Click a card').css('color','black');
+        } else {
+          $('#feedback h4').text('Game over. Thank you for playing.').css('color','red');
+        }
 
       }
     }
@@ -184,13 +200,18 @@ var UI = {
   // 6 6 6 6 6 6 6
   // move one level up
   levelUp: function(){
-    UI.resetLevelVariables();
-    $('#gameBoard').children('div').remove();
-    APP.level++;
-    UI.generateBoard(APP.level);
+    $('#feedback h4').text('Level end. Move to next level.').css('color','black');
+    setTimeout(function() {toNextLevel();},1000);
+    var toNextLevel = function(){
+      UI.resetLevelVariables();
+      $('#gameBoard').children('div').remove();
+      APP.level++;
+      UI.generateBoard(APP.level);
+    }
   },
   // 7 7 7 7 7 7
   updateSmileFace: function(){
+
     $('#chance').children('img').remove();
     for (var i=1; i<=APP.live; i++) {
       $('#chance').append(APP.smileImg);
@@ -211,6 +232,17 @@ var UI = {
 
 } // end of UI Object
 // ********************************************************
+
+// ========================================================
+var Data = {
+
+
+
+}
+
+
+// ========================================================
+
 
 
 // evoke the function
